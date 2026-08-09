05ab43751aee21cd71dc2836db0880b430796d96
MBODM <webmaster@mbodm.com>
2026-08-09 21:12:45 +0200

    dev...

--- release-win.bat
+++ release-win.bat
@@ -1,16 +1,15 @@
 @echo off
 
 cls
-set RELEASE=release\win
 
 echo.
 echo WOWCAM build script 1.0.0 (by MBODM 08/2026)
 echo.
-echo Will perform the following 4 steps now:
+echo Performing the following 4 steps now:
 echo.
 echo 1) clean projects
 echo 2) dotnet publish
-echo 3) copy 2 executables
+echo 3) copy both binaries
 echo 4) copy sample-config
 echo.
 
@@ -27,13 +26,13 @@ dotnet publish .\src\WOWCAM\WOWCAM.Windows\WOWCAM.Windows.csproj -c Release -v q
 if errorlevel 1 goto :error
 
 REM Copy the files
-if exist .\%RELEASE%  rmdir /s /q .\%RELEASE%
-mkdir .\%RELEASE%
-copy /B /V /Y .\src\WOWCAM\WOWCAM.Windows\bin\Release\net10.0-windows\win-x64\publish\WOWCAM.Windows.exe .\%RELEASE%\wowcamui.exe >NUL
-copy /B /V /Y .\src\WOWCAM\WOWCAM\bin\Release\net10.0\win-x64\publish\WOWCAM.exe .\%RELEASE%\wowcam.exe >NUL
-copy /B /V /Y .\wowcam.xml.sample .\%RELEASE% >NUL
+if exist .\release\win  rmdir /s /q .\release\win
+mkdir .\release\win
+copy /B /V /Y .\src\WOWCAM\WOWCAM.Windows\bin\Release\net10.0-windows\win-x64\publish\WOWCAM.Windows.exe .\release\win\wowcamui.exe >NUL
+copy /B /V /Y .\src\WOWCAM\WOWCAM\bin\Release\net10.0\win-x64\publish\WOWCAM.exe .\release\win\wowcam.exe >NUL
+copy /B /V /Y .\wowcam.xml.sample .\release\win >NUL
 
-echo Finished (you can now deploy everything inside the '%RELEASE%' folder)
+echo Finished (you can now deploy the content of the 'release\win' folder)
 echo.
 echo Have a nice day.
 goto :end
