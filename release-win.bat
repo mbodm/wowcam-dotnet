f2567c8d1e236637fff63f6dc8931c586eec8aec
MBODM <webmaster@mbodm.com>
2026-08-09 21:00:45 +0200

    dev ...

--- release-win.bat
+++ release-win.bat
@@ -1,41 +1,48 @@
 @echo off
 
 cls
+set RELEASE=release\win
 
 echo.
 echo WOWCAM build script 1.0.0 (by MBODM 08/2026)
 echo.
-echo Will perform the following 5 steps now:
+echo Will perform the following 4 steps now:
 echo.
-echo 1) project cleanup
+echo 1) clean projects
 echo 2) dotnet publish
-echo 3) copy executables
-echo 4) copy config
-echo 5) copy installer
+echo 3) copy 2 executables
+echo 4) copy sample-config
 echo.
 
-REM Publish the CLI tool
+REM Build CLI binary
 if exist .\src\WOWCAM\WOWCAM\bin rmdir /s /q .\src\WOWCAM\WOWCAM\bin
 if exist .\src\WOWCAM\WOWCAM\obj rmdir /s /q .\src\WOWCAM\WOWCAM\obj
-dotnet publish .\src\WOWCAM\WOWCAM\WOWCAM.csproj -c Release
-REM -v quiet
+dotnet publish .\src\WOWCAM\WOWCAM\WOWCAM.csproj -c Release -v quiet
+if errorlevel 1 goto :error
 
-REM Publish the Windows UI tool
+REM Build UI binary
 if exist .\src\WOWCAM\WOWCAM.Windows\bin rmdir /s /q .\src\WOWCAM\WOWCAM.Windows\bin
 if exist .\src\WOWCAM\WOWCAM.Windows\obj rmdir /s /q .\src\WOWCAM\WOWCAM.Windows\obj
-dotnet publish .\src\WOWCAM\WOWCAM.Windows\WOWCAM.Windows.csproj -c Release
+dotnet publish .\src\WOWCAM\WOWCAM.Windows\WOWCAM.Windows.csproj -c Release -v quiet
+if errorlevel 1 goto :error
 
-REM Deploy the files
-if not exist .\release mkdir .\release
-copy /B /V /Y .\src\WOWCAM\WOWCAM.Windows\bin\Release\net10.0-windows\publish\win-x64\WowcamWinUI.exe .\release\WOWCAMWINUI.exe >NUL
-copy /B /V /Y .\src\WOWCAM\WOWCAM\bin\Release\net10.0\publish\win-x64\wowcam.exe .\release\wowcam.exe >NUL
-copy /B /V /Y .\src\WOWCAM\WOWCAM\bin\Release\net10.0\publish\win-x64\wowcam.xml .\release\WOWCAM.xml >NUL
-copy /B /V /Y .\win-install\Install.bat .\release\Install.bat >NUL
+REM Copy the files
+if exist .\%RELEASE%  rmdir /s /q .\%RELEASE%
+mkdir .\%RELEASE%
+copy /B /V /Y .\src\WOWCAM\WOWCAM.Windows\bin\Release\net10.0-windows\win-x64\publish\WOWCAM.Windows.exe .\%RELEASE%\wowcamui.exe >NUL
+copy /B /V /Y .\src\WOWCAM\WOWCAM\bin\Release\net10.0\win-x64\publish\WOWCAM.exe .\%RELEASE%\wowcam.exe >NUL
+copy /B /V /Y .\wowcam.xml.sample .\%RELEASE% >NUL
 
-echo Finished (you can now deploy everything inside 'release' folder)
+echo Finished (you can now deploy everything inside the '%RELEASE%' folder)
 echo.
 echo Have a nice day.
+goto :end
 
+:error
+echo.
+echo Error: 'dotnet publish' failed
+
+:end
 REM Show timeout when started via double click
 REM (copied from https://stackoverflow.com/questions/5859854/detect-if-bat-file-is-running-via-double-click-or-from-cmd-window)
 if /I %0 EQU "%~dpnx0" timeout /T 9
